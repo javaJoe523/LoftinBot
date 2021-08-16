6bf9beff7fedaa928541151ee0dfe024ae7c51a4
Micah Loftin <micahbl@gmail.com>
2021-08-16 03:32:15 +0000

    Lots for refactoring to call commands dynamically and cut out a lot of the extra processing

--- helpers.py
+++ helpers.py
@@ -5,111 +5,89 @@ import random
 import const
 from datetime import date, datetime, timedelta
 from googleapiclient.discovery import build
-
+ 
 def get_quote(msg):
-  if (not allow_cmd(msg, ['!inspire'])):
-    return (['']) 
-  response = requests.get(const.INSPIRE_API_URL)
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return ([quote])
-
+ response = requests.get(const.INSPIRE_API_URL)
+ json_data = json.loads(response.text)
+ quote = json_data[0]['q'] + " -" + json_data[0]['a']
+ return ([quote])
+ 
 def get_fact(msg):
-  if (not allow_cmd(msg, ['!fact'])):
-    return (['']) 
-  response = requests.get(const.FACT_API_URL)
-  return ([response.text])
-
+ response = requests.get(const.FACT_API_URL)
+ return ([response.text])
+ 
 def get_space_pic(msg):
-  if (not allow_cmd(msg, ['!space'])):
-    return ([''])
-  startdate=date.today()
-  date_str=startdate-timedelta(random.randint(1,365))
-  api_key=os.getenv('NASA_API_KEY')
-  response = requests.get(f'{const.NASA_API_URL}?api_key={api_key}&date={date_str}')
-  json_data = json.loads(response.text)
-  return ([json_data['url']])
-
+ startdate=date.today()
+ date_str=startdate-timedelta(random.randint(1,365))
+ api_key=os.getenv('NASA_API_KEY')
+ response = requests.get(f'{const.NASA_API_URL}?api_key={api_key}&date={date_str}')
+ json_data = json.loads(response.text)
+ return ([json_data['url']])
+ 
 def date_diff(msg):
-  if (not allow_cmd(msg, const.CMD_DAYSUNTIL)):
-    return ([''])
-  msg = get_cmd_input(msg, '!daysuntil')
-  today = date.today()
-  to_date = datetime.strptime(msg, "%m/%d/%Y").date()
-  return ([(to_date - today).days])
-
+ msg = get_cmd_input(msg, '!daysuntil')
+ today = date.today()
+ to_date = datetime.strptime(msg, "%m/%d/%Y").date()
+ return ([(to_date - today).days])
+ 
 def translate_msg(msg, cmd, code):
-  # TODO: Use a dictionary for code
-  if (not allow_cmd(msg, [cmd])):
-    return ([''])
-  msg = get_cmd_input(msg, cmd)
-  if not msg:
-    return ([''])
-
-  service = build('translate', 'v2', developerKey=os.getenv('GOOGLE_API_KEY'))
-  json_data = service.translations().list( source='en', target=code, q=[msg] ).execute()
-  return ([json_data['translations'][0]['translatedText']])
-
+ msg = get_cmd_input(msg, cmd)
+ if not msg:
+   return ([''])
+ 
+ service = build('translate', 'v2', developerKey=os.getenv('GOOGLE_API_KEY'))
+ json_data = service.translations().list( source='en', target=code, q=[msg] ).execute()
+ return ([json_data['translations'][0]['translatedText']])
+ 
 def get_cur_weather(msg):
-  if (not allow_cmd(msg, ['!weather'])):
-    return ([''])
-  query = get_cmd_input(msg, '!weather')
-  
+ query = get_cmd_input(msg, '!weather')
   # Call the API
-  querystring = {"location":query,"aggregateHours":"24","shortColumnNames":"0","unitGroup":"us","contentType":"json"}
-  headers = {
-    'x-rapidapi-key': os.getenv('RAPIDAPI_KEY'),
-    'x-rapidapi-host': "visual-crossing-weather.p.rapidapi.com"
-  }
-  response = requests.get(
-    const.WEATHER_API_URL, 
-    headers=headers, 
-    params=querystring
-  )
-  json_data = json.loads(response.text)
-  
+ querystring = {"location":query,"aggregateHours":"24","shortColumnNames":"0","unitGroup":"us","contentType":"json"}
+ headers = {
+   'x-rapidapi-key': os.getenv('RAPIDAPI_KEY'),
+   'x-rapidapi-host': "visual-crossing-weather.p.rapidapi.com"
+ }
+ response = requests.get(
+   const.WEATHER_API_URL,
+   headers=headers,
+   params=querystring
+ )
+ json_data = json.loads(response.text)
   # Get specific data
-  try:
-    json_data = json_data['locations'][f'{query}']['currentConditions']
-  except:
-    print("Could not find currentConditions.")
-  cur_temp   = json_data['temp']
-  heat_index = json_data['heatindex']
-  windchill  = json_data['windchill']
-  humidity   = json_data['humidity']
-
-  # Create the message
-  result = f'The current temperature in {query} is {cur_temp}'
-  if (heat_index is not None):
-    result += f' but it feels like {heat_index}'
-  if (windchill is not None):
-    result += f' but there is a windchill of {windchill}'
-  result += f'. The current humidity is {humidity}%.'
-  return ([result])
-
+ try:
+   json_data = json_data['locations'][f'{query}']['currentConditions']
+ except:
+   print("Could not find currentConditions.")
+ cur_temp   = json_data['temp']
+ heat_index = json_data['heatindex']
+ windchill  = json_data['windchill']
+ humidity   = json_data['humidity']
+ 
+ # Create the message
+ result = f'The current temperature in {query} is {cur_temp}'
+ if (heat_index is not None):
+   result += f' but it feels like {heat_index}'
+ if (windchill is not None):
+   result += f' but there is a windchill of {windchill}'
+ result += f'. The current humidity is {humidity}%.'
+ return ([result])
+ 
 def roll_dice(msg):
-  if (not allow_cmd(msg, ['!dice'])):
-    return ([''])
-  return ([random.randint(2,12)])
-
+ return ([random.randint(2,12)])
+ 
 def get_sorting_house(msg):
-  if (not allow_cmd(msg, ['!sortinghat'])):
-    return ([''])
-  h = random.choice(const.HOUSES)
-  if (h == "Muggle"):
-    return (["I'm sorry but you are a muggle."])
-  else:
-    return([f'You are in house {h}.'])
+ h = random.choice(const.HOUSES)
+ if (h == "Muggle"):
+   return (["I'm sorry but you are a muggle."])
+ else:
+   return([f'You are in house {h}.'])
 
-def get_help():
-  return ([const.HELP_INFO])
-
-def allow_cmd(msg, options):
-  return (msg and any(c in msg for c in options))
-
+def has_option(msg, options):
+ return (msg and any(c in msg for c in options))
+ 
 def get_message_content(message):
-  return message.content.lower().strip() if (message and message.content) else message
-
+ return message.content.lower().strip() if (message and message.content) else message
+ 
 def get_cmd_input(msg, cmd):
-  pos = msg.lower().find(cmd) + len(cmd) + 1
-  return msg[pos::].strip()
+ pos = msg.lower().find(cmd) + len(cmd) + 1
+ return msg[pos::].strip()
--- main.py
+++ main.py
@@ -3,42 +3,64 @@ import os
 import random
 import const
 import helpers
-
+ 
 client = discord.Client()
-
-async def send_msg(message, keys, options, check_key=True):
-  # Remove empty options
-  options = [o for o in options if o]
-  # Check options and pick a random one
-  # TODO: Possibly use a decorator here
-  if options and (not check_key or helpers.allow_cmd(helpers.get_message_content(message), keys)):
-    response = random.choice(options)
-    await message.channel.send(response)
-
+ 
+async def send_cmd_msg(message, cmd, options):
+ # Remove empty options
+ options = [o for o in options if o]
+ if options:
+   response = random.choice(options)
+   await message.channel.send(response)
+ 
+async def send_chat_msg(message, keys, options):
+ # Remove empty options
+ msg = helpers.get_message_content(message)
+ options = [o for o in options if o]
+ if options and helpers.has_option(msg, keys):
+   response = random.choice(options)
+   await message.channel.send(response)
+ 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
+   print('We have logged in as {0.user}'.format(client))
+ 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   msg = helpers.get_message_content(message)
+  msg_commands = list(filter(lambda c: c in msg, const.COMMANDS))
+  # Define Commands so that they will be handled dynamically
+  cmd_func_list = {
+    '!hello': {'value': const.GREETING},
+    '!inspire': {'func': 'get_quote', 'args': {'msg': msg}},
+    '!8ball': {'value': const.YES_NO },
+    '!fact': {'func': 'get_fact', 'args': {'msg': msg}},
+    '!rps': {'value': const.RPS },
+    '!dice': {'func': 'roll_dice', 'args': {'msg': msg}},
+    '!french': {'func': helpers.translate_msg, 'args':{'msg': msg, 'cmd': '!french', 'code': 'fr'}},
+    '!spanish': {'func': helpers.translate_msg, 'args':{'msg': msg, 'cmd': '!spanish', 'code': 'es'}},
+    '!italian': {'func': helpers.translate_msg, 'args':{'msg': msg, 'cmd': '!italian', 'code': 'it'}},
+    '!daysuntil': {'func': 'date_diff', 'args': {'msg': msg}},
+    '!weather': {'func':'get_cur_weather', 'args':{'msg': msg}},
+    '!space': {'func':'get_space_pic', 'args':{'msg': msg}},
+    '!sortinghat': {'func':'get_sorting_house', 'args':{'msg': msg}},
+    '!help': {'value': [const.HELP_INFO] }
+  }
 
-  await send_msg(message, ['!hello'], const.GREETING)
-  await send_msg(message, ['!inspire'], helpers.get_quote(msg), False)
-  await send_msg(message, ['!8ball'], const.YES_NO)
-  await send_msg(message, const.SAD_WORDS, const.ENCOURAGEMENTS)
-  await send_msg(message, ['!fact'], helpers.get_fact(msg), False)
-  await send_msg(message, ['!rps'], const.RPS)
-  await send_msg(message, ['!dice'], helpers.roll_dice(msg), False)
-  await send_msg(message, ['!french'], helpers.translate_msg(msg, '!french', 'fr'), False)
-  await send_msg(message, ['!spanish'], helpers.translate_msg(msg, '!spanish', 'es'), False)
-  await send_msg(message, ['!italian'], helpers.translate_msg(msg, '!italian', 'it'), False)
-  await send_msg(message, ['!daysuntil'], helpers.date_diff(msg))
-  await send_msg(message, ['!weather'], helpers.get_cur_weather(msg), False)
-  await send_msg(message, ['!space'], helpers.get_space_pic(msg), False)
-  await send_msg(message, ['!help'], helpers.get_help())
-  await send_msg(message, ['!sortinghat'], helpers.get_sorting_house(msg), False)
+  # Handle Command Messages
+  for c in msg_commands:
+   assert cmd_func_list[c]
+   if cmd_func_list[c].get('value'):
+     await send_cmd_msg(message, c, cmd_func_list[c]['value'])
+   else:
+     func = cmd_func_list[c]['func']
+     kwargs = cmd_func_list[c]['args']
+     print('INFO: Possible API request for {0} for {1}'.format(func.__name__, c))
+     await send_cmd_msg(message, c, func(**kwargs))
 
+  # Handle Chat Messages
+  await send_chat_msg(message, const.SAD_WORDS, const.ENCOURAGEMENTS)
+ 
 client.run(os.getenv('DISCORD_TOKEN'))
